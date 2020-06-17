bf8064dbec5a921b52e2397bf2dfdc9bb5f90176
Thiago Brazileiro <thiago.brazileiro@hotmail.com>
2020-06-17 13:55:45 -0300

    :bug: Remove bug do drag and drop Remove o bug que fazia a agua ser transferida, de um jarro para o outro sem necessariamente arrastar e dropar um jarro no outro. Foi necessario colocar uma condiconal para evitar que ao mover um jarro sobre ele mesmo faça com que outro jarro transfira a agua.
    Tambem remove o bug que fazia uma quantidade excessiva de ações serem
    repetidas sem necessidade, foi necessario retirar um Listener de dentro
    do outro

--- src/controllers/controllerDragAndDrog.js
+++ src/controllers/controllerDragAndDrog.js
@@ -8,51 +8,54 @@ function allowDrop(ev){
 document.addEventListener("dragstart",function(event){
     
     jarroUsado = event.target.id;
-    console.log('Jarro usado = ' + jarroUsado);
+    console.log('Jarro usado = ' + jarroUsado);  
 
-    document.addEventListener("drop",function(event){
-        ferramentaUsada = event.target.id;
-        console.log('Ferramenta usada = ' +ferramentaUsada);
+},false);
 
-        if(ferramentaUsada == 'Ralo'){
-            if(jarroUsado == 'Jarro1'){
-                ralo.esvaziar(jarro1);
-                ferramentaUsada = "";
-                jarroUsado = "";
-            }else if(jarroUsado == 'Jarro2'){
-                ralo.esvaziar(jarro2);
-                ferramentaUsada = "";
-                jarroUsado = "";
-            }
-        }else if(ferramentaUsada == 'Torneira'){
-            if(jarroUsado == 'Jarro1'){
-                torneira.encher(jarro1);
-                ferramentaUsada = "";
-                jarroUsado = "";
-            }else if(jarroUsado == 'Jarro2'){
-                torneira.encher(jarro2);
-                ferramentaUsada = "";
-                jarroUsado = "";
-            }
-        }else if(ferramentaUsada == 'Jarro1'){
-          
-                console.log('JARRO2 JOGANDO AGUA NO JARRO1')
-                jarro2.transfereAgua(jarro1);
-                ferramentaUsada = "";
-                jarroUsado = "";
-            
+document.addEventListener("drop",function(event){
+    ferramentaUsada = event.target.id;
+    console.log('Ferramenta usada = ' +ferramentaUsada);
 
-        }else if(ferramentaUsada == 'Jarro2'){
-
-                console.log('JARRO1 JOGANDO AGUA NO JARRO2')
-                jarro1.transfereAgua(jarro2);
-                ferramentaUsada = "";
-                jarroUsado = "";
-                
-          
+    if(ferramentaUsada == 'Ralo'){
+        if(jarroUsado == 'Jarro1'){
+            ralo.esvaziar(jarro1);
+            ferramentaUsada = "";
+            jarroUsado = "";
+        }else if(jarroUsado == 'Jarro2'){
+            ralo.esvaziar(jarro2);
+            ferramentaUsada = "";
+            jarroUsado = "";
+        }
+    }else if(ferramentaUsada == 'Torneira'){
+        if(jarroUsado == 'Jarro1'){
+            torneira.encher(jarro1);
+            ferramentaUsada = "";
+            jarroUsado = "";
+        }else if(jarroUsado == 'Jarro2'){
+            torneira.encher(jarro2);
+            ferramentaUsada = "";
+            jarroUsado = "";
+        }
+    }else if(ferramentaUsada == 'Jarro1'){
+        if(jarroUsado == 'Jarro2'){
+            console.log('JARRO2 JOGANDO AGUA NO JARRO1')
+            jarro2.transfereAgua(jarro1);
+            ferramentaUsada = "";
+            jarroUsado = "";
         }
 
-       return false; 
-    });
-
-});
+    }else if(ferramentaUsada == 'Jarro2'){
+        if(jarroUsado == 'Jarro1'){
+            console.log('JARRO1 JOGANDO AGUA NO JARRO2')
+            jarro1.transfereAgua(jarro2);
+            ferramentaUsada = "";
+            jarroUsado = "";
+        }
+      
+    }
+    
+    
+    ferramentaUsada = "";
+    jarroUsado = "";
+    
+},false);
