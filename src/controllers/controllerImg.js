3862b9e3454f4965ddf7e6d4c0d9250b2ef35d0a
Thiago Brazileiro <thiago.brazileiro@hotmail.com>
2020-06-17 13:00:15 -0300

    :bug: Conserta co caminho para as imagens

--- src/controllers/controllerImg.js
+++ src/controllers/controllerImg.js
@@ -13,27 +13,27 @@ window.onload = setInterval(function(){
 
 
     if(jarro1.qntAtualAgua == jarro1.qntMaxAgua){
-        document.getElementById('Jarro1').src = '/views/img/copoCheio.png'
+        document.getElementById('Jarro1').src = 'img/copoCheio.png'
     }
 
     if(jarro1.qntAtualAgua < jarro1.qntMaxAgua && jarro1.qntAtualAgua > 0 ){
-        document.getElementById('Jarro1').src = '/views/img/copoMedio.png'
+        document.getElementById('Jarro1').src = 'img/copoMedio.png'
     }
 
     if(jarro1.qntAtualAgua == 0){
-        document.getElementById('Jarro1').src = '/views/img/copoVazio.png'
+        document.getElementById('Jarro1').src = 'img/copoVazio.png'
     }
 
     if(jarro2.qntAtualAgua == jarro2.qntMaxAgua){
-        document.getElementById('Jarro2').src = '/views/img/copoCheio.png'
+        document.getElementById('Jarro2').src = 'img/copoCheio.png'
     }
 
     if(jarro2.qntAtualAgua < jarro2.qntMaxAgua && jarro2.qntAtualAgua > 0 ){
-        document.getElementById('Jarro2').src = '/views/img/copoMedio.png'
+        document.getElementById('Jarro2').src = 'img/copoMedio.png'
     }
 
     if(jarro2.qntAtualAgua == 0){
-        document.getElementById('Jarro2').src = '/views/img/copoVazio.png'
+        document.getElementById('Jarro2').src = 'img/copoVazio.png'
     }
 
     //document.getElementById("AAA").innerHTML = ferramentaUsada ;
